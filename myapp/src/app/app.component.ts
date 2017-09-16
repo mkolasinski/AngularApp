7d5b13e95f4d9deca69bc7404ccaa9d19e470880
mkolasinski <m.kolasinski@poczta.fm>
2017-09-16 16:15:45 +0200

    - adding users.json

--- myapp/src/app/app.component.ts
+++ myapp/src/app/app.component.ts
@@ -1,30 +1,15 @@
-import { Component } from '@angular/core';
+import {Component} from "@angular/core";
+
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  // language=Angular2HTML
-  // template: "<h2> Hello {{ title }}</h2>"
 })
 export class AppComponent {
   title = 'Favourite App';
-  // name = " Michal ";
-  // lastname = " Kolasinski ";
-  // email = " m.kolasinski@gmail.com";
-  users: [
-    {
-      id: 1,
-      firstname: 'Michal',
-      lastname: 'Kolasinski',
-      email: 'm.kolasinski@gmail.com'
-    },
-    {
-      id:2,
-      firstname: 'Klaudia',
-      lastname: 'Hestkowska-Kolasinska',
-      email: 'klaudia.hestkowska@gmail.com'
-    }
-    ]
+  firstname = " Michal ";
+  lastname = " Kolasinski ";
+  email = " m.kolasinski@gmail.com";
 }
