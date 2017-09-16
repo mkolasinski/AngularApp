676c8a628712b73bbf79a1a052c3ac5e6a0d9465
mkolasinski <m.kolasinski@poczta.fm>
2017-09-16 09:44:35 +0200

    - repair a little of my project

--- myapp/src/app/app.component.ts
+++ myapp/src/app/app.component.ts
@@ -5,7 +5,12 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  // language=Angular2HTML
+  // template: "<h2> Hello {{ title }}</h2>"
 })
 export class AppComponent {
   title = 'Favourite App';
+  name = " Michal ";
+  lastname = " Kolasinski ";
+  email = " m.kolasinski@gmail.com";
 }
